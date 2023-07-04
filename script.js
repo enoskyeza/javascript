cb9a363d431d96f72288cf222f5a986389bd305f
enoskyeza <enoskyeza@gmail.com>
2023-07-04 21:43:24 +0300

    count app

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// initialize the count as 0
+// listen for clicks on the click button
+// add function that increments, and create it in js
+// change the count-el in html to reflect the new count
+
+let count=0;
+
+
+function increment() {
+    document.getElementById("count-el").innerText = count++
+}
+
+// my logger task
+
+// function num_caller() {
+//     console.log(42)
+// }
+
+// num_caller()
+
+// lap time addiyion task
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function lap_adder() {
+//     console.log(lap1 + lap2 + lap3 )
+// }
+
+// lap_adder()
