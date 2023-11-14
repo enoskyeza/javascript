de160164ec62f5162b77425192d0241a9a6510e1
enoskyeza <enoskyeza@gmail.com>
2023-11-14 23:59:59 +0300

    truthy and falsy concept in js

--- chromeextention/script.js
+++ chromeextention/script.js
@@ -30,3 +30,32 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
+
+
+// Testing Truthy & Falsy Concept
+// const credits = 0
+
+// if (credits) {
+//     console.log("Let's play 🎰")
+// } else {
+//     console.log("Sorry, you have no credits 😭")
+// }
+
+// truthy
+// falsy
+
+// false
+// 0
+// ""
+// null -> how you as a developer signalize emptiness
+// undefined -> how JavaScript signalizes emptiness
+// NaN
+
+let currentViewers = null
+
+currentViewers = ["jane", "nick"]
+
+if (currentViewers) {
+    // do something , e.g. notify the live streamers
+    console.log("we have viewers")
+}
