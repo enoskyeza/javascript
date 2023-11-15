1a04b3131e872055ac4c21becdc4170bd60368a0
enoskyeza <enoskyeza@gmail.com>
2023-11-15 17:33:31 +0300

    using args in functions

--- chromeextention/script.js
+++ chromeextention/script.js
@@ -2,19 +2,25 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-// Get the leads from the localStorage
-// Store it in a variable, leadsFromLocalStorage
-// Log out the variable
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
-
-    // To verify that it works:
-    console.log( localStorage.getItem("myLeads") )
 })
 
 function renderLeads() {
@@ -29,33 +35,4 @@ function renderLeads() {
         `
     }
     ulEl.innerHTML = listItems
-}
-
-
-// Testing Truthy & Falsy Concept
-// const credits = 0
-
-// if (credits) {
-//     console.log("Let's play 🎰")
-// } else {
-//     console.log("Sorry, you have no credits 😭")
-// }
-
-// truthy
-// falsy
-
-// false
-// 0
-// ""
-// null -> how you as a developer signalize emptiness
-// undefined -> how JavaScript signalizes emptiness
-// NaN
-
-let currentViewers = null
-
-currentViewers = ["jane", "nick"]
-
-if (currentViewers) {
-    // do something , e.g. notify the live streamers
-    console.log("we have viewers")
 }
