b1dd7d3ea625b5f173b0b048078925c359f4a1f5
enoskyeza <enoskyeza@gmail.com>
2023-11-16 18:16:36 +0300

    refactored code

--- basketball_scoreboard/script.js
+++ basketball_scoreboard/script.js
@@ -9,63 +9,119 @@ const guestScoreBtnTwo = document.getElementById('guest-score-btn-2')
 const guestScoreBtnThree = document.getElementById('guest-score-btn-3')
 const guestResetBtn = document.getElementById('reset-guest-score')
 
+//  refactored  function
+
+function updateScore(scoreElement, value) {
+    let newScore = parseInt(scoreElement.textContent)
+    if (value === 0){
+        scoreElement.textContent = 0
+    } else {
+        newScore += value
+        scoreElement.textContent = newScore
+    }
+}
 
 //  Home Buttons
+
 homeScoreBtnOne.addEventListener('click', function(){
-    addOne(homeScore)
+    updateScore(homeScore, 1)
 })
 
 homeScoreBtnTwo.addEventListener('click', function(){
-    addTwo(homeScore)
+    updateScore(homeScore, 2)
 })
 
 homeScoreBtnThree.addEventListener('click', function(){
-    addThree(homeScore)
+    updateScore(homeScore, 3)
 })
 
 homeResetBtn.addEventListener('click', function(){
-    resetScore(homeScore)
+    updateScore(homeScore, 0)
 })
 
 // Guest Buttons
 guestScoreBtnOne.addEventListener('click', function(){
-    console.log('Guest button onne clicked')
-    addOne(guestScore)
+    updateScore(guestScore, 1)
 })
 
 guestScoreBtnTwo.addEventListener('click', function(){
-    addTwo(guestScore)
+    updateScore(guestScore, 2)
 })
-
 guestScoreBtnThree.addEventListener('click', function(){
-    addThree(guestScore)
+    updateScore(guestScore, 3)
 })
 
 guestResetBtn.addEventListener('click', function(){
-    resetScore(guestScore)
+    updateScore(guestScore, 0)
 })
+
+
+
+
+
+
+
+
+
+// Old code
+
+//  Home Buttons
+
+// homeScoreBtnOne.addEventListener('click', function(){
+//     updateScore(homeScore, 1)
+// })
+
+// homeScoreBtnTwo.addEventListener('click', function(){
+//     addTwo(homeScore)
+// })
+
+// homeScoreBtnThree.addEventListener('click', function(){
+//     addThree(homeScore)
+// })
+
+// homeResetBtn.addEventListener('click', function(){
+//     resetScore(homeScore)
+// })
+
+// // Guest Buttons
+// guestScoreBtnOne.addEventListener('click', function(){
+//     console.log('Guest button onne clicked')
+//     addOne(guestScore)
+// })
+
+// guestScoreBtnTwo.addEventListener('click', function(){
+//     addTwo(guestScore)
+// })
+
+// guestScoreBtnThree.addEventListener('click', function(){
+//     addThree(guestScore)
+// })
+
+// guestResetBtn.addEventListener('click', function(){
+//     resetScore(guestScore)
+// })
 
 
 // Functions
 
-function addOne(score){
-    let newScore = parseInt(score.textContent)
-    newScore += 1
-    score.textContent = newScore
-}
+// function addOne(score){
+//     let newScore = parseInt(score.textContent)
+//     newScore += 1
+//     score.textContent = newScore
+// }
 
-function addTwo(score){
-    let newScore = parseInt(score.textContent)
-    newScore += 2
-    score.textContent = newScore
-}
+// function addTwo(score){
+//     let newScore = parseInt(score.textContent)
+//     newScore += 2
+//     score.textContent = newScore
+// }
 
-function addThree(score){
-    let newScore = parseInt(score.textContent)
-    newScore += 3
-    score.textContent = newScore
-}
+// function addThree(score){
+//     let newScore = parseInt(score.textContent)
+//     newScore += 3
+//     score.textContent = newScore
+// }
 
-function resetScore(score){
-    score.textContent = 0
-}
+// function resetScore(score){
+//     score.textContent = 0
+// }
