14c743f1e881b3bd34fc9be0c7e242d3b0e49f4c
enoskyeza <enoskyeza@gmail.com>
2023-11-16 23:06:41 +0300

    Configured realtime database for movie app

--- movieapp/script.js
+++ movieapp/script.js
@@ -1,12 +1,25 @@
+// An import example
+// import { add } from "../functions.js"
+
+// // Challenge: Make sure that add is working by console logging add with two number arguments.
+// console.log(add(1,1))
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const appSettings = {
     databaseURL: "https://playground-92113-default-rtdb.firebaseio.com/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, 'movies')
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-
+    push(moviesInDB, inputValue)
     console.log(`${inputValue} added to database`)
 })
